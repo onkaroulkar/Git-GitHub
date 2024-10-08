e8e606c438c072053678682fdbe0538878db0d4f
onkar oulkar <onkaroulkar@gmail.com>
2024-10-08 16:07:19 +0530

    new file

---
+++ DSA/variable.java
@@ -0,0 +1,10 @@
+package com.onkar;
+
+public class variable {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.print(x);
+        x = 20;
+        System.out.print(x);
+    }
+}
